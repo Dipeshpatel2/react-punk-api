b2862371e5f049d22d9412d74987894c23c0ab60
Dipeshpatel2e <dipeshpatel2@yahoo.co.uk>
2023-04-27 18:31:41 +0100

    all filters working

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import React, { useState, useEffect } from "react";
 import "./App.scss";
 import SideNav from "./components/SideNav/SideNav";
 import Main from "./components/Main/Main";
-//import beerData from "./assets/data/beers";
 
 const App = () => {
   const [beerInfo, setBeerInfo] = useState([]);
   const [classicRange, setClassicRange] = useState(false);
   const [highABV, setHighABV] = useState(false);
   const [highAcidity, setHighAcidity] = useState(false);
-  const [storeBeer, setStoreBeer] = useState([])
+  const [storeBeer, setStoreBeer] = useState([]);
 
   const getBeers = async () => {
     const url = "https://api.punkapi.com/v2/beers?page=1&per_page=80";
     const response = await fetch(url);
     const data = await response.json();
     setBeerInfo(data);
-    setStoreBeer(data)
+    setStoreBeer(data);
   };
 
   useEffect(() => {
@@ -32,50 +31,46 @@ const App = () => {
   };
 
   const filterBeers = (event) => {
-    console.log(event)
-    if (event.target.checked && event.target.id== "highABV") {
-    
-      const filteredBeers = beerInfo.filter((beer) => {
-          return beer.abv > 6;
-      });
-      setBeerInfo(filteredBeers);
-    }else if(event.target.checked && event.target.id== "highAcidity"){
-        const filteredBeers = beerInfo.filter((beer) => {
-          return beer.ph < 4; //&& beer.ph;       
-        });
-        setBeerInfo(filteredBeers);
-    }else  {
-  
-      setBeerInfo(storeBeer)
-    }
-
-  };
-
-  const handleCheckBoxHighABV = (event) => {
-    setHighABV(event.target.checked);
-    filterBeers(event);
-  };
-
-  const handleCheckBoxHighAcidity = (event) => {
-    setHighAcidity(event.target.checked);
-    filterBeers(event)
-    
-  };
-
-  const handleCheckBoxClassicRange = (event) => {
-    setClassicRange(event.target.checked);
-
+  console.log(event);
+  if (event.target.checked && event.target.id === "highABV") {
+    const filteredBeers = beerInfo.filter((beer) => {
+      return beer.abv > 6;
+    });
+    setBeerInfo(filteredBeers);
+  } else if (event.target.checked && event.target.id === "highAcidity") {
+    const filteredBeers = beerInfo.filter((beer) => {
+      return beer.ph < 4;
+    });
+    setBeerInfo(filteredBeers);
+  } else if (event.target.checked && event.target.id === "classicRange") {
     const filteredBeers = beerInfo.filter((beer) => {
       if (event.target.checked) {
         const [month, year] = beer.first_brewed.split("/");
-        return parseInt(month, year) < 2011;
+        return parseInt(year) < 2011;
       } else {
         return true;
       }
     });
-
     setBeerInfo(filteredBeers);
-  };
+  } else {
+    setBeerInfo(storeBeer);
+  }
+};
+
+const handleCheckBoxHighABV = (event) => {
+  setHighABV(event.target.checked);
+  filterBeers(event);
+};
+
+const handleCheckBoxHighAcidity = (event) => {
+  setHighAcidity(event.target.checked);
+  filterBeers(event);
+};
+
+const handleCheckBoxClassicRange = (event) => {
+  setClassicRange(event.target.checked);
+  filterBeers(event);
+};
 
   return (
     <div className="App">
@@ -91,6 +86,7 @@ const App = () => {
 };
 
 export default App;
+
 //===================================
 
 // import React, { useState, useEffect } from "react";
